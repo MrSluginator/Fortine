2db9b04c1e5d71e70f5c38f69ad3d37fda537ab2
“Fred D” <fred@dilger.ca>
2024-12-04 18:13:36 -0700

    Player Movement
    The movement input will now only result in the most recent direction being accepted, and will revert to the previous direction if the new one is released.

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 8.5
 const TILE_SIZE = 2
 
 var moving = false
@@ -16,20 +16,41 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		jump()
 
+	if Input.is_action_just_pressed("down-left"):
+		direction = Vector3(0, 0, 1)
+	elif Input.is_action_just_pressed("down-right"):
+		direction = Vector3(1, 0, 0)
+	elif Input.is_action_just_pressed("up-left"):
+		direction = Vector3(-1, 0, 0)
+	elif Input.is_action_just_pressed("up-right"):
+		direction = Vector3(0, 0, -1)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("up-left", "down-right", "up-right", "down-left")
-	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	if(not input_dir):
+		direction = Vector3.ZERO
+	elif (input_dir.length() == 1):
+		direction = Vector3(input_dir.x, 0, input_dir.y)
+
 	move()
+
 
 func move():
 	if direction:
 		if moving == false:
 			moving = true
 			var tween = create_tween()
+			tween.tween_property(self, "position.y", 0, 0)
 			tween.tween_property(self, "position", position + direction * TILE_SIZE, 0.35)
+			tween.tween_callback(done_move)
+
+func jump():
+	if moving == false:
+			moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + Vector3.UP * TILE_SIZE, 0.1)
 			tween.tween_callback(done_move)
 
 func done_move():
