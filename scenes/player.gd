1a78003f8901782e7f68d20609e7e958d50603d7
“Fred D” <fred@dilger.ca>
2024-12-04 18:57:12 -0700

    Player Movement
    Switched player movement to physics based. Added interaction force with 'J' to "kick" other RigidBody3D objects.

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,19 +4,25 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 8.5
 const TILE_SIZE = 2
+const STRENGTH = 400.0
 
 var moving = false
-var direction
-
+var direction = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if Input.is_action_just_pressed("kick") and $InteractRay.is_colliding():
+		var collider:Node3D = $InteractRay.get_collider()
+		if collider is RigidBody3D:
+			collider.apply_force(direction * STRENGTH)
+			print(collider.linear_velocity)
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		jump()
+		velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_just_pressed("down-left"):
 		direction = Vector3(0, 0, 1)
@@ -29,12 +35,16 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("up-left", "down-right", "up-right", "down-left")
-	if(not input_dir):
-		direction = Vector3.ZERO
-	elif (input_dir.length() == 1):
+	if (input_dir.length() == 1):
 		direction = Vector3(input_dir.x, 0, input_dir.y)
-
-	move()
+	if(not input_dir):
+		velocity = Vector3(0, velocity.y, 0)
+	else:
+		velocity = Vector3(0, velocity.y, 0) + direction * SPEED
+	
+	$InteractRay.target_position = direction
+	
+	move_and_slide()
 
 
 func move():
